f16d844eb8f388593499a743de1f7ea8c9b6c06e
Oscar-lizarraga <421088607@pcpuma.acatlan.unam.mx>
2021-10-03 08:02:18 -0500

    Funcion Ingresar y Mostrar

--- main.cpp
+++ main.cpp
@@ -13,119 +13,153 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include "usuario.h"
 
+void ImprimirResultado(int a);
 void ImprimirMenu();
-void LeerEntero(int *a);
-void LimpiarCadena(char *cadena);
+int RegistrarUsuario(Usuario usuario);
+int EliminarUsuario();
+int ModificarUsuario();
+int MostrarUsuarios();
 
 
-enum Sexo
-{
-	MASCULINO,
-	FEMENINO
-};
 
-typedef struct user 
-{
-	private:
-		int idUsuario;
-		char username[64];
-		char password[16];
-		char nombre[32];
-		char primerApellido[128];
-		char segundoApellido[128];
-		char email[128];
-		char telefono[16];
-		enum Sexo sexo;
-
-	public:
-		int Registrar()
-		{
-			printf("\n\t Ingrese el id: ");
-			LeerEntero(&idUsuario);
-
-			printf("\t Ingrese su nombre de usuario: ");
-			fgets(username,sizeof(username),stdin);
-			LimpiarCadena(username);
-
-			printf("\t Ingrese su contrasena: ");
-			fgets(password,sizeof(password),stdin);
-			LimpiarCadena(username);
-
-			printf("\t Ingrese su nombre: ");
-			fgets(nombre,sizeof(nombre),stdin);
-			LimpiarCadena(nombre);
-
-			printf("\t Ingrese su primer apellido: ");
-			fgets(primerApellido,sizeof(primerApellido),stdin);
-			LimpiarCadena(primerApellido);
-			
-			printf("\t Ingrese su segundo apellido: ");
-			fgets(segundoApellido,sizeof(segundoApellido),stdin);
-			LimpiarCadena(segundoApellido);
-
-			printf("\t Ingrese su e-mail: ");
-			fgets(email,sizeof(email),stdin);
-			LimpiarCadena(email);
-
-			printf("\t Ingrese su numero telefonico");
-			fgets(telefono,sizeof(telefono),stdin);
-			LimpiarCadena(telefono);
-
-			int aux_sexo;
-			do{
-				aux_sexo = 0;
-				printf("\t Sexo: \n\t [0] masculino \n\t [1] femenino \n\t Ingrese su tipo de sexo:");
-				LeerEntero(&aux_sexo);
-				if (aux_sexo == 0) sexo = MASCULINO;
-				if (aux_sexo == 1) sexo = FEMENINO;
-			}while(aux_sexo != 0 || aux_sexo != 1);
-
-			printf("%d",sexo);
-		}
-
-}Usuario;
 
 int main(int argc, char const *argv[])
 {
 	Usuario usuario;
-	usuario.Registrar();
+
+	int opcion;
+	do
+	{
+		ImprimirMenu();
+		LeerEntero(&opcion);
+		switch(opcion)
+		{
+			case 1:
+				usuario.DatosUsuario();
+				ImprimirResultado(RegistrarUsuario(usuario));
+				break;
+			case 2:
+				MostrarUsuarios();
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			default:
+				break;
+		}
+	} while (opcion < 1 || opcion >5);
 
 	return 0;
 }
 
+
+int RegistrarUsuario(Usuario usuario)
+{
+	FILE* file;
+	file = fopen("usuarios.dat","ab");
+
+	if(file == NULL)
+	{
+		return 0;
+	}
+	else
+	{
+		fwrite(&usuario, sizeof(usuario), 1, file);
+		fclose(file);
+		return 1;
+	}
+}
+
+int MostrarUsuarios()
+{
+	FILE* file;
+	Usuario *usuario;
+	Usuario aux_usuario;
+	int cantidad;
+
+	file = fopen("usuarios.dat","rb");
+	if(file == NULL)
+	{
+		return 0;
+	}
+	else
+	{
+		//posicionamos el puntero al final del archivo
+		fseek(file, 0, SEEK_END); 
+		
+		//obtenemos la cantidad de usuarios, ftell devuelve la cantidad de bits y lo dividimos entre la cantidad de usuarios
+		cantidad = ftell(file) / sizeof(Usuario); 
+		
+		//reservamos memoria para la cantidad de usuarios que hay
+		usuario = (Usuario*)malloc(cantidad * sizeof(Usuario));
+		
+		//posicionamos el puntero al inicio del archivo
+		fseek(file, 0, SEEK_SET);
+		
+		int i = 0;
+		while(!feof(file))
+		{
+			fread(&usuario[i],sizeof(usuario[i]),1,file);
+			i++;
+		}
+		printf("%s",usuario[0].nombre());
+		fclose(file);
+
+		return 1;
+	}
+}
+
+
+
+
+
+
+
+
 void ImprimirMenu()
 {
 	printf("\n\t --- Sistema de control de usuarios --- ");
-	printf("\n\n\t [1] Registrar nuevo usuario ");
-	printf("\n\n\t [1] Buscar datos de usuario ");
-	printf("\n\n\t [1] Eliminar usuario ");
-	printf("\n\n\t [1] Eliminar usuario ");
-	printf("\n\n\t [1] Modificar usuario ");
-	printf("\n\n\t [1] Mostrar todos los usuarios ");
+	printf("\n\t [1] Registrar nuevo usuario ");
+	printf("\n\t [1] Buscar datos de usuario ");
+	printf("\n\t [1] Eliminar usuario ");
+	printf("\n\t [1] Modificar usuario ");
+	printf("\n\t [1] Mostrar todos los usuarios ");
 }
 
-void LeerEntero(int *a)
+void ImprimirResultado(int a)
 {
-	char b;
-	while(scanf("%d%c",a,&b) != 2 || b != '\n' || *a < 0)
+	if (a)
 	{
-		printf("\t Ingreso mal el dato solicitado!, Vuelva a ingresarlo: ");
-		fflush(stdin);
+		printf("\n\t La operacion se realizo con exito");
+	}
+	else
+	{
+		printf("\n\t La operacion no se realizo correctamente");
 	}
 }
 
 
-//https://stackoverflow.com/questions/22065675/get-text-from-user-input-using-c
-//fgets will also pick-up the newline character \n at the end of input
-void LimpiarCadena(char *cadena)
-{
-	int longitud = strlen(cadena);
-	if (longitud > 0 && (cadena[longitud - 1] == '\n'))
-	{
-		cadena[longitud - 1] = '\0';
-	}
-}
+
+/*Consultas
+	Que es un enum 
+	https://www.geeksforgeeks.org/enumeration-enum-c/
+
+	fgets clean buffer
+	https://stackoverflow.com/questions/22065675/get-text-from-user-input-using-c
+
+	fwrite
+	https://www.tutorialspoint.com/c_standard_library/c_function_fwrite.htm
+
+	fseek
+	https://www.tutorialspoint.com/c_standard_library/c_function_fseek.htm
+*/
+
 
 
 
---
+++ usuario.h
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <string.h>
+
+void LeerEntero(int *a);
+void LimpiarCadena(char *cadena);
+
+enum Sexo
+{
+	MASCULINO,
+	FEMENINO
+};
+
+
+typedef struct user 
+{
+	public:
+		int _idUsuario;
+		char _username[64];
+		char _password[16];
+		char _nombre[32];
+		char _primerApellido[128];
+		char _segundoApellido[128];
+		char _email[128];
+		char _telefono[16];
+		enum Sexo _sexo;
+
+	public:
+
+		int isUsuario(){return this->_idUsuario;}
+		char* username(){return this->_username;}
+		char* password(){return this->_password;}
+		char* nombre(){return this->_nombre;}
+		char* primerApellido(){return this->_primerApellido;}
+		char* segundoApellido(){return this->_segundoApellido;}
+		char* email(){return this->_email;}
+		char* telefono(){return this->_telefono;}
+		Sexo sexo(){return this->_sexo;}
+
+
+
+		int DatosUsuario()
+		{
+			printf("\n\t Ingrese el id: ");
+			LeerEntero(&_idUsuario);
+
+			printf("\t Ingrese su nombre de usuario: ");
+			fgets(_username,sizeof(_username),stdin);
+			LimpiarCadena(_username);
+
+			printf("\t Ingrese su contrasena: ");
+			fgets(_password,sizeof(_password),stdin);
+			LimpiarCadena(_username);
+
+			printf("\t Ingrese su nombre: ");
+			fgets(_nombre,sizeof(_nombre),stdin);
+			LimpiarCadena(_nombre);
+
+			printf("\t Ingrese su primer apellido: ");
+			fgets(_primerApellido,sizeof(_primerApellido),stdin);
+			LimpiarCadena(_primerApellido);
+			
+			printf("\t Ingrese su segundo apellido: ");
+			fgets(_segundoApellido,sizeof(_segundoApellido),stdin);
+			LimpiarCadena(_segundoApellido);
+
+			printf("\t Ingrese su e-mail: ");
+			fgets(_email,sizeof(_email),stdin);
+			LimpiarCadena(_email);
+
+			printf("\t Ingrese su numero telefonico");
+			fgets(_telefono,sizeof(_telefono),stdin);
+			LimpiarCadena(_telefono);
+
+			int aux_sexo;
+			do{
+				aux_sexo = 0;
+				printf("\t Sexo: \n\t [0] masculino \n\t [1] femenino \n\t Ingrese su tipo de sexo:");
+				LeerEntero(&aux_sexo);
+				if (aux_sexo == 0) _sexo = MASCULINO;
+				if (aux_sexo == 1) _sexo = FEMENINO;
+			}while(aux_sexo != 0 && aux_sexo != 1);
+
+			return 0;
+		}
+
+}Usuario;
+
+
+void LeerEntero(int *a)
+{
+	char b;
+	while(scanf("%d%c",a,&b) != 2 || b != '\n' || *a < 0)
+	{
+		printf("\t Ingreso mal el dato solicitado!, Vuelva a ingresarlo: ");
+		fflush(stdin);
+	}
+}
+
+void LimpiarCadena(char *cadena)
+{
+	int longitud = strlen(cadena);
+	if (longitud > 0 && (cadena[longitud - 1] == '\n'))
+	{
+		cadena[longitud - 1] = '\0';
+	}
+}
