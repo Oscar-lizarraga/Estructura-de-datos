fc219d2879e273f10fb40f3c198fb30e045c2a2b
Oscar-lizarraga <421088607@pcpuma.acatlan.unam.mx>
2021-10-03 05:38:21 -0500

    Modificaciones a la funcion ingresar

--- main.cpp
+++ main.cpp
@@ -8,30 +8,30 @@
  * Valades Herrera Elisa
  * Ferrer Flores Ximena Mailyn
  * Cruz Mendez Yuliana Vianney 
- khknn
  * 
  * *********************************************************/
 
 #include <stdio.h>
+#include <string.h>
 
 void ImprimirMenu();
-void LeerCadena();
-int LeerEntero();
+void LeerEntero(int *a);
+void LimpiarCadena(char *cadena);
 
 
 enum Sexo
 {
-	masculino,
-	femenino
+	MASCULINO,
+	FEMENINO
 };
 
 typedef struct user 
 {
 	private:
 		int idUsuario;
-		char username[32];
+		char username[64];
 		char password[16];
-		char nombre[128];
+		char nombre[32];
 		char primerApellido[128];
 		char segundoApellido[128];
 		char email[128];
@@ -39,20 +39,58 @@ typedef struct user
 		enum Sexo sexo;
 
 	public:
-		int registrar()
+		int Registrar()
 		{
 			printf("\n\t Ingrese el id: ");
-			idUsuario = LeerEntero();
-			printf("\n\t Ingrese su nombre de usuario");
-			fgets(username, sizeof(username), stdin);
+			LeerEntero(&idUsuario);
 
+			printf("\t Ingrese su nombre de usuario: ");
+			fgets(username,sizeof(username),stdin);
+			LimpiarCadena(username);
+
+			printf("\t Ingrese su contrasena: ");
+			fgets(password,sizeof(password),stdin);
+			LimpiarCadena(username);
+
+			printf("\t Ingrese su nombre: ");
+			fgets(nombre,sizeof(nombre),stdin);
+			LimpiarCadena(nombre);
+
+			printf("\t Ingrese su primer apellido: ");
+			fgets(primerApellido,sizeof(primerApellido),stdin);
+			LimpiarCadena(primerApellido);
+			
+			printf("\t Ingrese su segundo apellido: ");
+			fgets(segundoApellido,sizeof(segundoApellido),stdin);
+			LimpiarCadena(segundoApellido);
+
+			printf("\t Ingrese su e-mail: ");
+			fgets(email,sizeof(email),stdin);
+			LimpiarCadena(email);
+
+			printf("\t Ingrese su numero telefonico");
+			fgets(telefono,sizeof(telefono),stdin);
+			LimpiarCadena(telefono);
+
+			int aux_sexo;
+			do{
+				aux_sexo = 0;
+				printf("\t Sexo: \n\t [0] masculino \n\t [1] femenino \n\t Ingrese su tipo de sexo:");
+				LeerEntero(&aux_sexo);
+				if (aux_sexo == 0) sexo = MASCULINO;
+				if (aux_sexo == 1) sexo = FEMENINO;
+			}while(aux_sexo != 0 || aux_sexo != 1);
+
+			printf("%d",sexo);
 		}
 
 }Usuario;
 
 int main(int argc, char const *argv[])
 {
-	/* code */
+	Usuario usuario;
+	usuario.Registrar();
+
 	return 0;
 }
 
@@ -67,15 +105,28 @@ void ImprimirMenu()
 	printf("\n\n\t [1] Mostrar todos los usuarios ");
 }
 
-int LeerEntero()
+void LeerEntero(int *a)
 {
-	int n; char a;
-	while(scanf("%d%c",&n,&a) != 2 || a != '\n' || n < 0)
+	char b;
+	while(scanf("%d%c",a,&b) != 2 || b != '\n' || *a < 0)
 	{
-		printf("\n\t Ingreso mal el dato solicitado!");
+		printf("\t Ingreso mal el dato solicitado!, Vuelva a ingresarlo: ");
 		fflush(stdin);
 	}
-	return n;
 }
+
+
+//https://stackoverflow.com/questions/22065675/get-text-from-user-input-using-c
+//fgets will also pick-up the newline character \n at the end of input
+void LimpiarCadena(char *cadena)
+{
+	int longitud = strlen(cadena);
+	if (longitud > 0 && (cadena[longitud - 1] == '\n'))
+	{
+		cadena[longitud - 1] = '\0';
+	}
+}
+
+
 
 
